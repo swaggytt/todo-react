2b1106b869c19f2b83a446d5da53336646ca89a0
swaggytt <ichromex@hotmail.com>
2021-02-05 18:51:17 +0700

    todoCardComplete

--- frontend/src/components/card/index.jsx
+++ frontend/src/components/card/index.jsx
@@ -1,18 +1,44 @@
 import "./Card.scss";
 import { useState } from "react";
+import Calender from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 const Card = () => {
+  const [text, setText] = useState("");
+  const [stateCalender, setStateCalender] = useState(false);
+  const [taskDate, setTaskDate] = useState(new Date());
 
-const [text,setText] = useState("")
+  // console.log(taskDate);
 
   return (
     <div className="card">
-      <input
-        type="text"
-        value={text}
-        placeholder="Do something..."
-        onChange={(e) => setText(e.target.value)}
-      ></input>
+      <div className="content">
+        <input
+          type="text"
+          value={text}
+          placeholder="Do something..."
+          onChange={(e) => setText(e.target.value)}
+        ></input>
+        <button>
+          <i className="fas fa-check-circle check"></i>
+        </button>
+      </div>
+      <div className="calender-input">
+        <button
+          onClick={() => setStateCalender(!stateCalender)}
+          className="calendar-btn"
+        >
+          <i className="fas fa-calendar-minus calender-font"></i>
+        </button>
+        {stateCalender && (
+          <Calender className='calendar-pop' onClickDay={(value)=>{setStateCalender(false);setTaskDate(value)}} value={taskDate}></Calender>
+        )}
+      </div>
+      {new Date(taskDate).toLocaleString("th-TH", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      })}
     </div>
   );
 };
