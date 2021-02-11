3cdfaa05c016c2885fd4b849f136107a1297e01e
swaggytt <ichromex@hotmail.com>
2021-02-11 14:40:04 +0700

    Decorate contactPage

--- frontend/src/containers/contactPage/contactPage.jsx
+++ frontend/src/containers/contactPage/contactPage.jsx
@@ -1,5 +1,29 @@
+import "./contactpage.scss";
+
 const ContactPage = () => {
-  return <div>My contact</div>;
+  return (
+    <div className="card-contact">
+      <span className="avatar">
+        <img
+          src="https://sv1.picz.in.th/images/2021/02/10/ot42sE.jpg"
+          alt="avatar"
+        />
+      </span>
+      <h1>TAE</h1>
+      <p>COMPUTER ENGINEERING STUDENT</p>
+      <div className="btn-link">
+        <a href="https://www.facebook.com/taetae2323" target="_blank">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="https://github.com/swaggytt" target="_blank">
+          <i class="fab fa-github"></i>
+        </a>
+        <a href="https://instagram.com/t_g9h" target="_blank">
+          <i class="fab fa-instagram"></i>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
