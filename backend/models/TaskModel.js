ff07509c1b5827f45cc8cb75c4ed97ffde0632af
swaggytt <ichromex@hotmail.com>
2021-02-11 13:41:33 +0700

    add timestamps in database

--- backend/models/TaskModel.js
+++ backend/models/TaskModel.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
-const TaskSchema = mongoose.Schema({
-  taskName: String,
-  isFinished: { type: Boolean, default: false },
-  taskDate: Date,
-});
+const TaskSchema = mongoose.Schema(
+  {
+    taskName: String,
+    isFinished: { type: Boolean, default: false },
+    taskDate: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const TaskModel = mongoose.model("Task", TaskSchema);
 
